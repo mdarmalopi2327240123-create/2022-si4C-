58c8e54d8ce50d19419adcf54e50f4c13cac45d0
mdarmalopi2327240123-create <mdarmalopi_2327240123@mhs.mdp.ac.id>
2026-04-07 13:01:56 +0700

    update new
    update baru

--- localstorage/scriptls.js
+++ localstorage/scriptls.js
@@ -1,51 +1,64 @@
 let npm = document.getElementById("npm");
 let nama = document.getElementById("nama");
+let gambar = document.getElementById("gambar");
 
 function simpan() {
-    console.log(npm.value)
-    console.log(nama.value)
+    console.log(npm.value);
+    console.log(nama.value);
+    console.log(gambar.value);
 
-    // localStorage.setItem("npm", npm.value)
-    // localStorage.setItem("nama", nama.value)
+    // localStorage.setItem("npm", npm.value);
+    // localStorage.setItem("nama", nama.value);
+    // localStorage.setItem("gambar", gambar.value);
 
-    // jika local storage belum ada isi/value
-    if(localStorage.getItem("mahasiswa")===null) {
-        // simpan array kosong [] 
-        localStorage.setItem("mahasiswa", "[]")
+    //cek localstorage sudah ada isi atau belum
+    if (localStorage.getItem("mahasiswa") === null) {
+        //simpan array kosong []
+        localStorage.setItem("mahasiswa", "[]");
     }
-
-    // panggil local storage (konversi string => object)
+    //panggil localstorage (konversi string => object)
     let data = JSON.parse(localStorage.getItem("mahasiswa"))
     console.log(data)
-    
-    // simpan value npm dan nama ke dalam object data
+
+    //simpan value npm dan nama ke dalam object data
     data.push({
-        npm: npm.value, 
-        nama: nama.value
+        npm: npm.value,
+        nama: nama.value,
+        gambar: gambar.value
     })
     console.log(data)
 
-    // simpan data terbaru ke dalam local storage
-    // konversi dari object menjadi string
-    localStorage.setItem("mahasiswa", JSON.stringify(data))
+    //simpan data terbaru ke dalam localstorage
+    //konversi object menjadi string
+    localStorage.setItem("mahasiswa", JSON.stringify(data));
 
-    // panggil tampil()
+    //panggil tampil()
     tampil()
 }
 
 function tampil() {
-    // panggil dulu local storage
+    //panggil dulu local storage
     let hasil = JSON.parse(localStorage.getItem("mahasiswa"))
 
-    // clear element ul id=list-mhs
+    //clear element ul id=list-mhs
     document.getElementById("list-mhs").innerHTML = ""
-
-    // lakukan perulangan (forEach)
+    //lakukan perulangan (foreach)
     hasil.forEach(element => {
-        // console.log(element)
-        document.getElementById("list-mhs").innerHTML += `<li>${element.npm} ${element.nama}</li>`
+        //console.log(element)
+        document.getElementById("list-mhs").innerHTML += 
+        `<div class="col-lg-4 col-md-6 col-sm-12">
+        <img src="${element.gambar}" alt="Gambar URL" class="img-fluid">
+        <h4 class="text-primary text-center">${element.npm}</h4>
+        <h6 class="text-success text-center">${element.nama}</h6>
+        </div>`
+        //`<li>${element.npm} - ${element.nama}</li>`
     });
 }
 
-// jalankan function tampil()
+//jalankan fungsi tampil()
 tampil()
+
+//latihan
+//tambah 1 input image URL di bawah input nama
+// simpan ke local storage
+// tampilkan
